4841b9544a4fa5c01d30b2b4a91950ce2b2329f6
Famila <familafami99@outlook.com>
2021-05-16 13:46:53 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Sidebar from './sidebar';
+import Topbar from './topbar';
+import Dashboard from './dashboard/dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    
+    <div id="wrapper">
+      <Sidebar></Sidebar>
+      <div id="content-wrapper" class="d-flex flex-column">
+       <div id="content">
+         <Topbar></Topbar>
+         <div class="container-fluid">
+           <Dashboard></Dashboard>
+           <div><footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright © Your Website 2021</span>
+                    </div>
+                </div>
+            </footer></div>
+         </div>
+       </div>
+      </div>
     </div>
   );
 }
---
+++ src/dashboard/dashboard.js
@@ -0,0 +1,25 @@
+import Card from "./card";
+import Chart from "./chart";
+import Projectcard from "./projectcard";
+
+export default function Dashboard() {
+    return <>
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+            <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+            class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+    </div>
+    <div class="row">
+        <Card></Card>
+     </div>
+     <div class="row">
+         <Chart></Chart>
+     </div>
+     <div class="row">
+         <Projectcard></Projectcard>
+     </div>
+    
+
+     
+    </>
+}
